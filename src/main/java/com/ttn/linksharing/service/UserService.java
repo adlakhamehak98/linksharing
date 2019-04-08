397573e13bfe087e0cc1db6fe87968afff4f8b51
Mehak Adlakha <adlakhamehak98@gmail.com>
2019-04-08 12:44:20 +0530

    custom 404, 500 error pages added

--- src/main/java/com/ttn/linksharing/service/UserService.java
+++ src/main/java/com/ttn/linksharing/service/UserService.java
@@ -1,6 +1,5 @@
 package com.ttn.linksharing.service;
 
-import com.ttn.linksharing.entity.User;
 import com.ttn.linksharing.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,8 +8,4 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     UserRepository userRepository;
-//
-//    public void userDataSave(){
-//        userRepository.save();
-//    }
 }
