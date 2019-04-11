ac892e37d6f1703a3bf935d31f904a403fdb1273
Mehak Adlakha <adlakhamehak98@gmail.com>
2019-04-11 21:52:14 +0530

    subscription card ajax call seriousness visibility delete

--- src/main/java/com/ttn/linksharing/controller/DashboardController.java
+++ src/main/java/com/ttn/linksharing/controller/DashboardController.java
@@ -4,6 +4,7 @@ import com.ttn.linksharing.entity.*;
 import com.ttn.linksharing.enums.Visibility;
 import com.ttn.linksharing.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
@@ -100,6 +102,30 @@ public class DashboardController {
         }
         return "redirect:/dashboard";
     }
+
+//    @RequestMapping(value = "/dashboard/sendInvitation", method = RequestMethod.POST)
+//    public String createTopic( ModelMap model, HttpSession session) {
+//        Integer userId = (Integer) session.getAttribute("loggedInUser");
+//        if (userId != null) {
+//            User user = userService.findById(userId);
+//            ModelAndView modelAndView = new ModelAndView("Dashboard");
+//            return "redirect:/dashboard";
+//        }
+//        return "redirect:/dashboard";
+//    }
+//
+//    private void sendEmail (User user) throws Exception {
+//        System.out.println("Herre>>>>>>>>>>>>>>>>>>>>>>");
+//        MimeMessage message = sender.createMimeMessage();
+//        MimeMessageHelper helper = new MimeMessageHelper(message);
+//        helper.setTo(user.getEmail());
+//        System.out.println(user.getEmail());
+//        System.out.println("Herre>>>>>>>>>>>>>>>>>>>>>>");
+//
+//        helper.setText("Hello your password is: " + user.getPassword());
+//        helper.setSubject("Please Find your password here");
+//        sender.send(message);
+//    }
 
     @RequestMapping(value = "/dashboard/shareDocument", method = RequestMethod.POST)
     public String shareDocument(@RequestParam MultipartFile file, @ModelAttribute("documentResource") DocumentResource resource, ModelMap model, BindingResult bindingResult, HttpSession session) {
